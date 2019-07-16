7ef729f3ab7c3412d3fa9f98f48e4ce502a2be10
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-07-16 10:02:53 -0700

    Add project files.

---
+++ ModelsAndModelBinding/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsAndModelBinding.Models
+{
+    public class Student
+    {
+        public int MyProperty { get; set; }
+        /// <summary>
+        /// The legal name. First Last
+        /// ex. J Doe
+        /// </summary>
+        [Required(ErrorMessage = "Gimme that name!")]
+        [Display(Name = "Full Legal Name")]
+        public string FullName { get; set; }
+        /// <summary>
+        /// The program the student intends 
+        /// to complete
+        /// </summary>
+
+        [Required]
+        [Display(Name = "Choose Program")]
+        public string ProgramOfChoice { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        //Max 30 chars just for demo
+        [StringLength(30, MinimumLength = 5)]
+        public string HomeAddress { get; set; }
+    }
+}
